875ab052b92e5adee057e2b0476ffad6aaf3cdd0
Yasmin Macena <yasmacx@gmail.com>
2026-06-22 20:16:19 -0300

    Add age calculation function in adivinhar_idade.py

---
+++ adivinhar_idade.py
@@ -0,0 +1,8 @@
+def calcular_idade():
+
+    atual = int(input("Digite o ano atual: "))
+    idade = int(input("Digite o ano de nascimento: "))
+    calcular = atual - idade
+    print(f"Sua idade é: {calcular} anos!")
+
+calcular_idade()
