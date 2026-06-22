4d495746d5b86a0bc874f9ca5304201c2195d457
Yasmin Macena <yasmacx@gmail.com>
2026-06-22 20:16:42 -0300

    Add functions to count words and letters

---
+++ contador_caracter.py
@@ -0,0 +1,11 @@
+def contar_palavras():
+    return len(palavras)
+
+palavras = input("Digite uma frase: ").split()
+print(f'A frase que você digitou possui {contar_palavras()} palavras.')
+
+def contar_letras():
+    return len(palavra)
+
+palavra = input("Digite uma palavra: ")
+print(f'A palavra que você digitou possui {contar_letras()} letras.')
